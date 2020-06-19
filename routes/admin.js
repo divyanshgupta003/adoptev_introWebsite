289adc24b6752171e670449e3fb6a099f00f1ea5
divyanshgupta003 <guptadivyansh588@gmail.com>
2020-06-20 02:04:52 +0530

    enterprise page changed

--- routes/admin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const logisticsController = require('../controllers/logistics_controller');
-
-router.get('/7867' , logisticsController.admin);
-router.post('/send-price' , logisticsController.sendPrice);
-
-module.exports = router;
